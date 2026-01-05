1e3456fb349525430815c1ee76e98862bc8f4003
taboonchai1991-ops <taboonchai1991@gmail.com>
2026-01-05 14:34:46 +0700

    Create amulet-data.js

---
+++ js/amulet-data.js
@@ -0,0 +1,27 @@
+const amulets = [
+  {
+    name: "พระสามกรุเวียงท่ากานเชียงใหม่",
+    url: "https://photos.app.goo.gl/NdjgmtvmYvhDnP1v7"
+  },
+  {
+    name: "พระเครื่องกรุงเทพมหานคร",
+    url: "https://photos.app.goo.gl/GcmfNfKeiCzaTsFH8"
+  },
+  {
+    name: "พระเครื่องเมืองอยุธยา",
+    url: "https://photos.app.goo.gl/4XQMKJAjMaJmFzZAA"
+  },
+  {
+    name: "หลวงปู่ไข่วัดเชิงเลน กทม",
+    url: "https://icedrive.net/s/Q5S1Yi6twu4xQTwf58gR3DPRY3uy"
+  },
+  {
+    name: "พระปรุหนังหลวงพ่อเนียม",
+    url: "https://mega.nz/folder/bK4yjIaA#iLp1GuhiwRVVpPshQ1YpXQ"
+  },
+  {
+    name: "พระปิดตาหลวงพ่อเนียม",
+    url: "https://mega.nz/folder/aTp1VYZC#N3tGa1QYQO7YDpLXTtGnAQ"
+  }
+  // 👉 เพิ่มได้เรื่อย ๆ
+];
