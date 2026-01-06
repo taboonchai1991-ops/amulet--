c34787f4c9cf4e7d9dd53d748fb1e41a87a72488
taboonchai1991-ops <taboonchai1991@gmail.com>
2026-01-06 13:26:42 +0700

    Update amulet-data.js

--- js/amulet-data.js
+++ js/amulet-data.js
@@ -34,7 +34,7 @@ const amulets = [
     keywords: ["นครนายก", "ปทุมธานี"]
   },
   {
-    name: "หลวงปู่ไข่วัดเชิงเลน กทม",
+    name: "หลวงปู่ไข่ วัดเชิงเลน",
     url: "https://icedrive.net/s/Q5S1Yi6twu4xQTwf58gR3DPRY3uy",
     keywords: ["หลวงปู่ไข่", "วัดเชิงเลน", "ไข่"]
   },
@@ -61,7 +61,7 @@ const amulets = [
   {
     name: "พระรอดมหาวันพิมพ์กลาง",
     url: "https://photos.app.goo.gl/SHUqBow99Rx5Ar1R6",
-    keywords: ["พระรอด", "มหาวัน"]
+    keywords: ["พระรอด", "มหาวัน", "พิมพ์กลาง"]
   },
   {
     name: "พระเครื่องนครปฐม",
@@ -74,18 +74,48 @@ const amulets = [
     keywords: ["พระรอด", "พิมพ์ใหญ่"]
   },
   {
-    name: "พระพุทโธน้อยแม่ชีบุญเรือน",
+    name: "พระพุทโธน้อย แม่ชีบุญเรือน",
     url: "https://icedrive.net/s/vYkgSS3vTYAvVbkA97vSTki4tfXF",
     keywords: ["พุทโธน้อย", "แม่ชีบุญเรือน"]
   },
   {
-    name: "หลวงพ่อเกษรวัดท่าพระ",
+    name: "หลวงพ่อเกษร วัดท่าพระ",
     url: "https://icedrive.net/s/jb992PRiWg2htBuxaB4bZ1f6R7i8",
     keywords: ["หลวงพ่อเกษร", "วัดท่าพระ"]
   },
   {
-    name: "พระปรุหนังหลวงพ่อเนียม",
+    name: "พระปรุหนัง หลวงพ่อเนียม",
     url: "https://mega.nz/folder/bK4yjIaA#iLp1GuhiwRVVpPshQ1YpXQ",
     keywords: ["ปรุหนัง", "หลวงพ่อเนียม", "เนียม"]
+  },
+  {
+    name: "หลวงพ่อแก้ว พิมพ์ปั่น",
+    url: "https://icedrive.net/s/kB2a6552k1QyT4bzhaGYtB1F5fT7",
+    keywords: ["หลวงพ่อแก้ว", "พิมพ์ปั่น"]
+  },
+  {
+    name: "หลวงพ่อวงษ์ วัดทุ่งผักกูด",
+    url: "https://icedrive.net/s/kB2a6552k1QyT4bzhaGYtB1F5fT7",
+    keywords: ["หลวงพ่อวงษ์", "วัดทุ่งผักกูด"]
+  },
+  {
+    name: "หลวงปู่พริ้ง วัดบางปะกอก",
+    url: "https://icedrive.net/s/1Bjz968YDNt3YaiQ8tBR1FTW3gX8",
+    keywords: ["หลวงปู่พริ้ง", "หลวงพ่อพริ้ง"]
+  },
+  {
+    name: "พระกรุวังบัว เพชรบุรี",
+    url: "https://icedrive.net/s/3i8uCCFhB2Z5F4xv1tx2uikikYTa",
+    keywords: ["พระกรุวังบัว", "กรุวังบัว", "เพชรบุรี"]
+  },
+  {
+    name: "พระพิมพ์เศียรแหลม หลวงพ่อเนียม",
+    url: "https://mega.nz/folder/GGRmEajK#0fctrh7UpyNlGaZU5Yftwg",
+    keywords: ["เศียรแหลม", "หลวงพ่อเนียม"]
+  },
+  {
+    name: "พระเชตุพน หลวงพ่อเนียม",
+    url: "https://icedrive.net/s/9f4SPuf1Yvg5C8D4Q3u9vg1YBNW4",
+    keywords: ["พระเชตุพน", "หลวงพ่อเนียม"]
   }
 ];
